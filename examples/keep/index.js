9d8404bad4d768a336d78d7136260a29a9814467
Yuki Kodama <endflow.net@gmail.com>
2015-12-27 03:43:56 +0900

    Move common modules

--- examples/keep/index.js
+++ examples/keep/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './app';
-import store from './store';
+import store from '../common/store';
 
 ReactDOM.render(
   <Provider store={store}>
--- examples/simple/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import reducer from './reducers';
-import logger from 'redux-logger';
-import { middleware as tooltip } from '../../src/index';
-
-export default applyMiddleware(
-  tooltip, logger()
-)(createStore)(reducer);
