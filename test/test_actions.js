83565522b941f3f92faec069ca953ad6cc5a8a2e
Yuki Kodama <endflow.net@gmail.com>
2015-12-30 15:47:20 +0900

    Add test for actions

---
+++ test/test_actions.js
@@ -0,0 +1,56 @@
+import assert from 'power-assert';
+import * as actions from '../src/actions';
+
+describe('actions', () => {
+  describe('.delay', () => {
+    context('with action contains no meta data', () => {
+      const action = {
+        type: 'WOO',
+        payload: {
+          value: 123
+        },
+      };
+
+      it('appends meta data for delay feature', () => {
+        assert.deepStrictEqual(actions.delay(action), {
+          type: 'WOO',
+          payload: {
+            value: 123
+          },
+          meta: {
+            delay: 1500,
+          }
+        });
+      });
+    });
+
+    context('with action contains other meta data', () => {
+      const action = {
+        type: 'RUBY',
+        payload: {
+          on: 'rails',
+        },
+        meta: {
+          gem: 'bundler',
+        }
+      };
+
+      it('does not modify other meta data', () => {
+        assert.deepStrictEqual(actions.delay(action), {
+          type: 'WOO',
+          payload: {
+            value: 123
+          },
+          type: 'RUBY',
+          payload: {
+            on: 'rails',
+          },
+          meta: {
+            gem: 'bundler',
+            delay: 1500,
+          }
+        });
+      });
+    });
+  });
+});
