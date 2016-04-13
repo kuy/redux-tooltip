a38a3962a4fb41e1f01a0b297a810c7f4ba41b41
Yuki Kodama <endflow.net@gmail.com>
2016-04-13 14:32:35 +0900

    Add test to reproduce issue #19

---
+++ tests/unit/test_reducer.js
@@ -0,0 +1,31 @@
+import assert from 'power-assert';
+import sinon from 'sinon';
+import reducer from '../../src/reducer';
+import * as actions from '../../src/actions';
+
+describe('reducer', () => {
+  let warn;
+  beforeEach(() => {
+    warn = sinon.spy(console, 'warn');
+  });
+
+  afterEach(() => {
+    console.warn.restore();
+  });
+
+  it("warns with 'el' prop", () => {
+    reducer(undefined, actions.show({ el: 'abc' }));
+    assert(warn.calledOnce);
+    assert(warn.firstCall.args[0] === "DEPRECATED: Use 'origin' instead of 'el' in props for Tooltip component or 'show' action.");
+  });
+
+  it("doesn't warn with 'origin' prop", () => {
+    reducer(undefined, actions.show({ origin: 'abc' }));
+    assert(warn.callCount === 0);
+  });
+
+  it("ignores non-related actions which contains 'el' prop in payload", () => {
+    reducer(undefined, { type: 'NON_RELATED_ACTION', payload: { el: 'abc' } });
+    assert(warn.callCount === 0);
+  });
+});
