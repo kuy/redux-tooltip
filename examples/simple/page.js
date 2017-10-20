ae5f1501ad2a7e11124da2ac21100c134ff9a86e
Charimon <acharkin@gmail.com>
2017-10-20 07:56:40 -0700

    updated to use prop-types

--- examples/simple/page.js
+++ examples/simple/page.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Origin } from '../../src/index';
 
 const PIXEL = 'data:image/gif;base64,R0lGODlhAQABAPAAAAD/AP///yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==';
